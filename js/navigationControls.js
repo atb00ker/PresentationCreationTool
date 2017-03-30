400a36bcb0b97b6ff3174e94f4f947076c3934be
ATB00ker <ajay39in@gmail.com>
2017-03-30 14:57:09 +0530

    Navigation Controls Efficiency
    -Removed Jquery.js
    -Changed the navigation to system to a working automatic system.
    - Introduced img. insertion basic file.
    - Added Slides

--- js/navigationControls.js
+++ js/navigationControls.js
@@ -1,83 +1,131 @@
-xPageOneSet=1;
-xPageLastSet=2;
-yPageOneSet=0;
-yPageLastSet=0;
-x=2;
-y=0;
-refreshOnClick();
+var slides = [[0,0],[1,0],[2,0],[2,1],[3,0]];
+var userPosition=0;
+var currentPositionX=0;
+var currentPositionY=0;
+document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides	[userPosition][1]).classList.add("slideActive");
+slideExistence();
+/*This Section is to check the existence of current slide in all the directions*/
+function slideExistence() {
+/*Slide Right Check*/
+	for(x=userPosition;x<slides.length;x++){
+		if(slides[userPosition][1]!=0){
+			slideExistenceRight = false;
+			document.getElementById("navControlsRight").classList.add("buttonPassive");
+			break;
+		}		
+		else if(slides[userPosition][0]<slides[x][0]){
+			slideExistenceRight = true;
+			document.getElementById("navControlsRight").classList.remove("buttonPassive");
+			break;
+		}
+		else {
+			slideExistenceRight = false;
+			document.getElementById("navControlsRight").classList.add("buttonPassive");
+		}
+	}
+/*Slide Left Check*/
+	for(x=userPosition;x>=0;x--){
+		if(slides[userPosition][1]!=0){
+			slideExistenceLeft = false;
+			document.getElementById("navControlsLeft").classList.add("buttonPassive");
+			break;
+		}		
+		else if(slides[userPosition][0]>slides[x][0]){
+			slideExistenceLeft = true;
+			document.getElementById("navControlsLeft").classList.remove("buttonPassive");
+			break;
+		}
+		else {
+			slideExistenceLeft = false;
+			document.getElementById("navControlsLeft").classList.add("buttonPassive");
+		}
+	}
+/*Slide Up Check*/
+	for(x=userPosition;x<slides.length;x++){
+		if(slides[userPosition][0]==slides[x][0]){
+			if(slides[userPosition][1]<slides[x][1]){
+				slideExistenceUp = true;
+				document.getElementById("navControlsUp").classList.remove("buttonPassive");
+				break;
+			}
+			else {
+			slideExistenceUp = false;
+			document.getElementById("navControlsUp").classList.add("buttonPassive");
+			}
+		}
+		else {
+			slideExistenceUp = false;
+			document.getElementById("navControlsUp").classList.add("buttonPassive");
+		}
+	}
+/*Slide Down Check*/
+	for(x=userPosition;x>=0;x--){
+		if(slides[userPosition][0]==slides[x][0]){
+			if(slides[userPosition][1]>slides[x][1]){
+				slideExistenceDown = true;
+				document.getElementById("navControlsDown").classList.remove("buttonPassive");
+				break;
+			}
+			else {
+			slideExistenceDown = false;
+			document.getElementById("navControlsDown").classList.add("buttonPassive");
+			}
+		}
+		else {
+			slideExistenceDown = false;
+			document.getElementById("navControlsDown").classList.add("buttonPassive");
+		}
+	}
+}
+/*Pressing Button Actions
+These functions need Slide existence in all direction in work.*/
 function buttonClickRight() {
-	if(xPageLastSet>x){
-		++x;
-		refreshOnClick();
+	if(slideExistenceRight == true) {
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.remove("slideActive");
+			do{
+			userPosition++;
+			}while(slides[userPosition][1]!=0);//Forwards the slide until next slide in X-axis is found.
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.add("slideActive");
+		slideExistence();
 	}
 }
 function buttonClickLeft() {
-	if(xPageOneSet<x){
-	   --x;
-	   refreshOnClick();
+	if(slideExistenceLeft == true) {
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.remove("slideActive");
+			do{
+			userPosition--;
+			}while(slides[userPosition][1]!=0);//Forwards the slide until next slide in X-axis is found.
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.add("slideActive");
+		slideExistence();
 	}
 }
 function buttonClickUp() {
-	if(yPageLastSet>y){
-		++y;
-		refreshOnClick();
+	if(slideExistenceUp == true){
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.remove("slideActive");
+		currentPositionY=slides[userPosition][1];
+			do{
+			userPosition++;
+			}while(currentPositionY>=slides[userPosition][1]);//Works till it finds a next slide in Y-axis.
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.add("slideActive");
+		slideExistence();
 	}
 }
 function buttonClickDown() {
-	if(yPageLastSet<y){
-	   --y;
-	   refreshOnClick();
-	}
+	if(slideExistenceDown == true){
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.remove("slideActive");
+		currentPositionY=slides[userPosition][1];
+		do{
+		userPosition--;
+		}while(slides[userPosition][1]>=currentPositionY);//Works till it finds a next slide in Y-axis.
+		document.getElementById("slideX"+slides[userPosition][0]+"Y"+slides[userPosition][1]).classList.add("slideActive");
+		slideExistence();
+	}	
 }
-function refreshOnClick() {
-/**************************
-* X-Axis Updation Section
-**************************/
-	if(x==1){
-		$("#secondPage").removeClass("active");
-		$("#homePage").addClass("active");
-	}
-	if(x==2){
-		$("#homePage").removeClass("active");
-		$("#secondPage").addClass("active");
-	}
-/**************************
-* Y-Axis Updation Section
-**************************/	
-
-/**************************
-* Button Updattion Handle
-**************************/	
-	if(xPageLastSet==x){
-		$("#navControlsRight").addClass("buttonPassive");
-		$("#navControlsRight").removeClass("buttonActive");
-	}
-	if(xPageLastSet!=x){
-		$("#navControlsRight").removeClass("buttonPassive");
-		$("#navControlsRight").addClass("buttonActive");	
-	}
-	if(xPageOneSet==x){
-		$("#navControlsLeft").addClass("buttonPassive");
-		$("#navControlsLeft").removeClass("buttonActive");
-	}
-	if(xPageOneSet!=x){
-		$("#navControlsLeft").removeClass("buttonPassive");
-		$("#navControlsLeft").addClass("buttonActive");	
-	}
-	if(yPageLastSet==y){
-		$("#navControlsDown").addClass("buttonPassive");
-		$("#navControlsDown").removeClass("buttonActive");
-	}
-	if(yPageLastSet!=y){
-		$("#navControlsDown").removeClass("buttonPassive");
-		$("#navControlsDown").addClass("buttonActive");	
-	}
-	if(yPageOneSet==y){
-		$("#navControlsUp").addClass("buttonPassive");
-		$("#navControlsUp").removeClass("buttonActive");
-	}
-	if(yPageOneSet!=y){
-		$("#navControlsUp").removeClass("buttonPassive");
-		$("#navControlsUp").addClass("buttonActive");	
-	}
+/*Call the button action function from input via keyboard*/
+window.onkeyup = function(e) {
+   var key = e.keyCode;
+   if (key == 38) {buttonClickUp();}
+   else if (key == 40) {buttonClickDown(); }
+   else if (key == 39) {buttonClickRight();}
+   else if (key == 37) {buttonClickLeft();}
 }
